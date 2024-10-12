7922b6bbb1bd48796f08ad00e1bc7857140aab7b
Abhinav Tushar <abhinav@lepisma.xyz>
2024-10-12 14:14:17 +0530

    Add auto conversion of .note to .pdf while syncing

--- supernote-sync.py
+++ supernote-sync.py
@@ -2,22 +2,39 @@
 directory at regular frequency.
 
 Usage:
-  supernote-sync.py <output-dir> --url=<url> [--poll-interval=<poll-interval>]
+  supernote-sync.py <output-dir> --url=<url> [--poll-interval=<poll-interval>] [--note-to-pdf]
 
 Options:
   --url=<url>                          Full URL for supernote web browsing tool
   --poll-interval=<poll-interval>      Duration (in s) to sleep between sync [default: 600]
+  --note-to-pdf                        Whether to convert all .note files to pdf after downloading
 """
 
 from docopt import docopt
 import aiohttp
+import aiofiles
 import asyncio
 import re
 import json
 import os
 from loguru import logger
+import supernotelib as sn
+from supernotelib.converter import PdfConverter
+
 
 __version__ = "0.1.1"
+
+async def convert_to_pdf(input_path: str, output_path: str):
+    """Convert input .note file to output .pdf file.
+    """
+
+    notebook = sn.load_notebook(input_path, policy="strict")
+    converter = PdfConverter(notebook, palette=None)
+
+    data = converter.convert(-1, vectorize=True, enable_link=True, enable_keyword=True)
+
+    async with aiofiles.open(output_path, "wb") as fp:
+        await fp.write(data)
 
 
 async def download_file(url: str, output_path: str):
@@ -48,7 +65,7 @@ async def read_directory(session: aiohttp.ClientSession, url: str) -> list[dict]
         return data["fileList"]
 
 
-async def worker(q, session, root_url, semaphore, output_dir):
+async def worker(q, session, root_url, semaphore, output_dir, should_convert: bool):
     while True:
         item = await q.get()
         item_url = root_url + item["uri"]
@@ -64,10 +81,15 @@ async def worker(q, session, root_url, semaphore, output_dir):
                 await download_file(item_url, filepath)
                 logger.info(f"Downloaded {item['name']} to {filepath}")
 
+                if should_convert and filepath.endswith(".note"):
+                    pdf_filepath = os.path.splitext(filepath)[0] + ".pdf"
+                    await convert_to_pdf(filepath, pdf_filepath)
+                    logger.info(f"Converted {item['name']} to pdf")
+
         q.task_done()
 
 
-async def supernote_to_local(session: aiohttp.ClientSession, root_url: str, output_dir: str):
+async def supernote_to_local(session: aiohttp.ClientSession, root_url: str, output_dir: str, should_convert: bool):
     """Run supernote to local sync. This unconditionally downloads the
     supernote tree structure to the local directory.
 
@@ -84,7 +106,7 @@ async def supernote_to_local(session: aiohttp.ClientSession, root_url: str, outp
 
     tasks = []
     for _ in range(n):
-        task = asyncio.create_task(worker(q, session, root_url, semaphore, output_dir))
+        task = asyncio.create_task(worker(q, session, root_url, semaphore, output_dir, should_convert))
         tasks.append(task)
 
     await q.join()
@@ -98,11 +120,12 @@ async def main():
     root_url = args["--url"]
     output_dir = args["<output-dir>"]
     poll_interval = int(args["--poll-interval"])
+    should_convert = args["--note-to-pdf"]
 
     async with aiohttp.ClientSession() as session:
         while True:
             try:
-                await supernote_to_local(session, root_url, output_dir)
+                await supernote_to_local(session, root_url, output_dir, should_convert)
             except aiohttp.client_exceptions.ClientConnectorError:
                 logger.error("Can't connect to supernote")
 
